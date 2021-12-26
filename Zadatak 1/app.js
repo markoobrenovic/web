f9f8cc2f415ade344404d2ee8b615a15edc98ee3
markoobrenovic <marko.obrenovic.lucani@gmail.com>
2021-12-26 20:26:08 +0100

    Add files via upload

---
+++ Zadatak 1/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var userId = $("#todoUser").val();
+    var fromTodo = $("#fromTodo").val();
+    var toTodo = $("#toTodo").val();
+
+
+    $("#userBtn").click(function(){
+        var to = $("#todoUser").val();
+        var fromTodo = $("#fromTodo").val();
+        var toTodo= $("#toTodo").val();
+    })
+
+    $("#todoBtn").click(function(){
+        var todoUser = $("#todoUser").val();
+        var fromTodo = $("#fromTodo").val();
+        var toTodo= $("#toTodo").val();
+    })
+})
+
+
+
+
+  
